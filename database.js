7b06e8bae9fa0337ae286970fabeb7889ba817f3
fernrey <Fernando39@live.com>
2022-05-04 00:42:58 -0400

    new changes

--- database.js
+++ database.js
@@ -4,37 +4,37 @@ var csvtojson = require('csvtojson')
 const DBSOURCE = "db.sqlite"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
-        console.log('Connected to the SQLite database.')
+    if(err) {
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
+        console.log('Connected to SQLite database.')
         db.run(`CREATE TABLE cars (
-            Car_ID INTEGER PRIMARY KEY,
-            Judge_ID INTEGER, 
-            Judge_Name text,
-            Racer_Turbo INTEGER,
-            Racer_Supercharged INTEGER,
-            Racer_Performance INTEGER
-            )`,
+            Car_ID INT PRIMARY KEY,
+            Judge_ID INT,
+            Judge_Name TEXT,
+            Racer_Turbo INT,
+            Racer_Supercharged INT,
+            Racer_Performance INT
+        )`),
         (err) => {
-            if (err) {
+            if(err) {
                 // Table already created
-            }else{
-                csvtojson().fromFile("./d66a59b6db4e59c16efd4c42ad411f8e/data.csv")
+            } else {
+                csvtojson.fromFile('./d66a59b6db4e59c16efd4c42ad411f8e/data.csv')
                 .then(data => {
-                    let insert = 'INSERT INTO cars (Car_ID, Judge_ID, Judge_Name, Racer_Turbo, Racer_Supercharged, Racer_Performance) VALUES (??????)';
-
+                    let insert = 'INSERT INTO cars (Car_ID, Judge_ID, Judge_Name, Racer_Turbo, Racer_Supercharged, Racer_Performance) VALUES (?,?,?,?,?,?)'
+                    
                     for(const item of data) {
-                        db.run(insert, [item.Car_ID, item.Judge_ID, item.Judge_Name, item.Racer_Turbo, item.Racer_Supercharged, item.Racer_Performance]);
+                        db.run(insert, [item.Car_ID, item.Judge_ID, item.Judge_Name, item.Racer_Turbo, item.Racer_Supercharged, item.Racer_Performance])
                     }
                 }).catch(err => {
                     // log error if any
-                    console.log(err);
+                    console.log(err)
                 });
+            }
         }
-    })
-}
-
+    }
+});
 module.exports = db
